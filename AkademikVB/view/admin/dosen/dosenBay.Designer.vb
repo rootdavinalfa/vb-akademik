14d6272ed01509a7b8cd3d25bad0b5f968339a25
davin <dbasudewa@gmail.com>
2019-12-08 21:26:22 +0700

    Dosen Create OK

--- AkademikVB/view/admin/dosen/dosenBay.Designer.vb
+++ AkademikVB/view/admin/dosen/dosenBay.Designer.vb
@@ -22,9 +22,189 @@ Partial Class dosenBay
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dgvDosen = New System.Windows.Forms.DataGridView()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblNID = New System.Windows.Forms.Label()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.inputNama = New System.Windows.Forms.TextBox()
+        Me.inputFakultas = New System.Windows.Forms.ComboBox()
+        Me.inputProdi = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvDosen, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnRefresh)
+        Me.GroupBox1.Controls.Add(Me.dgvDosen)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(683, 204)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "List Dosen"
+        '
+        'dgvDosen
+        '
+        Me.dgvDosen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDosen.Location = New System.Drawing.Point(7, 42)
+        Me.dgvDosen.Name = "dgvDosen"
+        Me.dgvDosen.Size = New System.Drawing.Size(670, 156)
+        Me.dgvDosen.TabIndex = 0
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(602, 13)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.btnRefresh.TabIndex = 1
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.inputProdi)
+        Me.GroupBox2.Controls.Add(Me.inputFakultas)
+        Me.GroupBox2.Controls.Add(Me.inputNama)
+        Me.GroupBox2.Controls.Add(Me.btnCreate)
+        Me.GroupBox2.Controls.Add(Me.btnGenerate)
+        Me.GroupBox2.Controls.Add(Me.lblNID)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(20, 235)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(676, 203)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Create Dosen"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fakultas"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Prodi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(5, 146)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "NID"
+        '
+        'lblNID
+        '
+        Me.lblNID.AutoSize = True
+        Me.lblNID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNID.Location = New System.Drawing.Point(69, 146)
+        Me.lblNID.Name = "lblNID"
+        Me.lblNID.Size = New System.Drawing.Size(58, 20)
+        Me.lblNID.TabIndex = 4
+        Me.lblNID.Text = "%%nid&"
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(326, 143)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(94, 23)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate NID"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(564, 163)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(94, 23)
+        Me.btnCreate.TabIndex = 5
+        Me.btnCreate.Text = "Create"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'inputNama
+        '
+        Me.inputNama.Location = New System.Drawing.Point(71, 32)
+        Me.inputNama.Name = "inputNama"
+        Me.inputNama.Size = New System.Drawing.Size(249, 20)
+        Me.inputNama.TabIndex = 6
+        '
+        'inputFakultas
+        '
+        Me.inputFakultas.FormattingEnabled = True
+        Me.inputFakultas.Location = New System.Drawing.Point(71, 66)
+        Me.inputFakultas.Name = "inputFakultas"
+        Me.inputFakultas.Size = New System.Drawing.Size(249, 21)
+        Me.inputFakultas.TabIndex = 7
+        '
+        'inputProdi
+        '
+        Me.inputProdi.FormattingEnabled = True
+        Me.inputProdi.Location = New System.Drawing.Point(71, 96)
+        Me.inputProdi.Name = "inputProdi"
+        Me.inputProdi.Size = New System.Drawing.Size(249, 21)
+        Me.inputProdi.TabIndex = 8
+        '
+        'dosenBay
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(703, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "dosenBay"
         Me.Text = "dosenBay"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.dgvDosen, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents dgvDosen As DataGridView
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblNID As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents inputProdi As ComboBox
+    Friend WithEvents inputFakultas As ComboBox
+    Friend WithEvents inputNama As TextBox
 End Class
--- AkademikVB/view/admin/dosen/dosenBay.vb
+++ AkademikVB/view/admin/dosen/dosenBay.vb
@@ -1,3 +1,137 @@
 ﻿Public Class dosenBay
+    Dim con As OleDb.OleDbConnection
+    Private Sub dosenBay_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con = connect()
+        initInputFakultas()
+        refresh()
+    End Sub
 
+    Private Sub initDGV()
+        dgvDosen.Rows.Clear()
+        dgvDosen.Columns.Clear()
+        dgvDosen.Columns.Add("nim", "NID")
+        dgvDosen.Columns.Add("namaha", "Nama")
+    End Sub
+
+
+
+    Private Sub initInputFakultas()
+        Try
+            Dim dss = New DataSet
+            Dim sqlListFakultas = "SELECT fakultasList.idFakultas, fakultasList.NamaFakultas
+                                   FROM fakultasList;
+                                   "
+            Dim ole = con.CreateCommand()
+            ole.CommandText = sqlListFakultas
+            Dim da As New OleDb.OleDbDataAdapter(ole)
+            da.Fill(dss)
+            With inputFakultas
+                .DataSource = dss.Tables(0)
+                .ValueMember = "idFakultas"
+                .DisplayMember = "NamaFakultas"
+            End With
+
+            AddHandler inputFakultas.SelectedIndexChanged, AddressOf inputFakultas_Selected
+            initInputProdi()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub initInputProdi()
+        Try
+            Dim dss = New DataSet
+            Dim sqlListFakultas = "SELECT jurusanList.idFakultas, jurusanList.idJurusan, jurusanList.NamaJurusan
+                                   FROM jurusanList
+                                   WHERE (((jurusanList.idFakultas)=@1));
+                                   "
+            Dim ole = con.CreateCommand()
+            ole.CommandText = sqlListFakultas
+            ole.Parameters.Add(New OleDb.OleDbParameter("@1", inputFakultas.SelectedValue))
+            Dim da As New OleDb.OleDbDataAdapter(ole)
+            da.Fill(dss)
+            With inputProdi
+                .DataSource = dss.Tables(0)
+                .ValueMember = "idJurusan"
+                .DisplayMember = "NamaJurusan"
+            End With
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub inputFakultas_Selected(sender As Object, e As EventArgs)
+        If inputFakultas.Items.Count > 0 Then
+            initInputProdi()
+        End If
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim Generator As System.Random = New System.Random()
+        Dim lastYear = DateTime.Now.ToString("yy")
+        Dim indexx = GetRandom(Generator, 1, 999)
+        Dim generateNim = String.Format("0{0}0{1}{2}{3}1", inputFakultas.SelectedValue, inputProdi.SelectedValue, lastYear, indexx)
+        lblNID.Text = generateNim
+    End Sub
+    Private Sub refresh()
+        Try
+            initDGV()
+            Dim dss = New DataSet
+            Dim sql = "SELECT NID,Nama FROM dosenList;
+                        "
+            Dim ole = con.CreateCommand()
+            ole.CommandText = sql
+            Dim da As New OleDb.OleDbDataAdapter(ole)
+            da.Fill(dss)
+            For i = 0 To dss.Tables(0).Rows.Count - 1
+                Dim nid = dss.Tables(0).Rows(i).Item(0)
+                Dim nama = dss.Tables(0).Rows(i).Item(1)
+
+                dgvDosen.Rows.Add(nid, nama)
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        refresh()
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+        If inputNama.Text.Count <> 0 Then
+            Try
+                Dim Sql = "INSERT INTO dosenList( NID, Nama) VALUES(@1,@2);"
+                Dim ole = con.CreateCommand()
+                ole.CommandText = Sql
+                ole.Parameters.Add(New OleDb.OleDbParameter("@1", lblNID.Text))
+                ole.Parameters.Add(New OleDb.OleDbParameter("@2", inputNama.Text))
+                If ole.ExecuteNonQuery() Then
+                    insertPassword()
+                End If
+            Catch ex As OleDb.OleDbException
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            MsgBox("Nama perlu diisi kemudian generate nim")
+        End If
+    End Sub
+
+    Private Sub insertPassword()
+        Try
+            Dim password As String = "123"
+            Dim Sql = "INSERT INTO userLoginDosen([ID],[password]) VALUES(@1,@2);"
+            Dim ole = con.CreateCommand
+            ole.CommandText = Sql
+            ole.Parameters.Add(New OleDb.OleDbParameter("@1", lblNID.Text))
+            ole.Parameters.Add(New OleDb.OleDbParameter("@2", password))
+            If ole.ExecuteNonQuery() Then
+                MessageBox.Show("Data Created!")
+                refresh()
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
 End Class
